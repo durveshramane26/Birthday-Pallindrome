727cebd4cb7a99b0e85356e58267050f154fa7c8
durveshramane26 <durveshramane6@gmail.com>
2022-07-14 20:32:04 +0530

    Checking leap year

--- app.js
+++ app.js
@@ -62,3 +62,18 @@ function checkPalindromeForAllFromate(date) {
 
     return flag;
 }
+
+
+//Checking leapyear
+function leapyear(year) {
+    if(year % 400 === 0) {
+        return true
+    }
+    if(year % 100 === 0){
+        return false
+    }
+    if(year % 4 === 0){
+        return true
+    }
+    return false;
+}
